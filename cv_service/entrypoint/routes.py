285d9ac6819f2ccce348f74f89ee12f7a773c967
khatriamit <rajukhatri054@gmail.com>
2022-09-24 10:36:39 +0545

    create language route

--- cv_service/entrypoint/routes.py
+++ cv_service/entrypoint/routes.py
@@ -6,6 +6,7 @@ from cv_service.services.handlers import (
     PersonalInfoViewSet,
     EducationInfoViewSet,
     WorkInfoViewSet,
+    LanguageInfoViewSet,
 )
 
 router = DefaultRouter()
@@ -13,6 +14,7 @@ router.register("job_info", JobInfoViewSet, basename="job_info")
 router.register("personal_info", PersonalInfoViewSet, basename="personal_info")
 router.register("education_info", EducationInfoViewSet, basename="education_info")
 router.register("work_info", WorkInfoViewSet, basename="work_info")
+router.register("language_info", LanguageInfoViewSet, basename="language_info")
 
 urlpatterns = [
     path("", include(router.urls)),
