769becadefcbbbe5286a16303cd57e518066a41d
khatriamit <rajukhatri054@gmail.com>
2022-09-24 10:01:51 +0545

    add endpoints for respective handlers

--- cv_service/entrypoint/routes.py
+++ cv_service/entrypoint/routes.py
@@ -1,9 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from profile_service.services.handlers import ProfileView
+from app.models import JobInfo
+from cv_service.services.handlers import (
+    JobInfoViewSet,
+    PersonalInfoViewSet,
+    EducationInfoViewSet,
+    WorkInfoViewSet,
+)
 
 router = DefaultRouter()
-router.register("profile", ProfileView, basename="profile")
+router.register("job_info", JobInfoViewSet, basename="job_info")
+router.register("personal_info", PersonalInfoViewSet, basename="personal_info")
+router.register("education_info", EducationInfoViewSet, basename="education_info")
+router.register("work_info", WorkInfoViewSet, basename="work_info")
 
 urlpatterns = [
     path("", include(router.urls)),
